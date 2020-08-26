3ac3e25c8bd6d54a8fc76a7952ee03ed8145d177
ssudler <ssudler21@gmail.com>
2020-08-25 20:28:34 -0400

    Use GameContext for listing players

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,20 +1,24 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { SocketContext } from '../contexts/Contexts';
+import React, { useContext, useEffect } from 'react';
+import { GameContext, SocketContext } from '../contexts/Contexts';
 import Player from './Player';
 import Svg from './Svg';
 import gamepad from '../assets/img/icons/theme/devices/gamepad-2.svg';
 
 const Players = () => {
   const { socket } = useContext(SocketContext) || {};
-  // TODO: Use GameContext instead of local variable
-  const [players, setPlayers] = useState([]);
+  const { gameState } = useContext(GameContext) || {};
+  const [gameSettings, setGameSettings] = gameState || [];
+  const { players } = gameSettings || {};
 
   useEffect(() => {
     let subscribed = true;
 
     const handlePlayerUpdate = (allPlayers) => {
       if (subscribed) {
-        setPlayers(allPlayers);
+        setGameSettings((settings) => ({
+          ...settings,
+          players: allPlayers,
+        }));
       }
     };
     socket.emit('player loaded');
@@ -26,7 +30,7 @@ const Players = () => {
     };
   }, []);
 
-  if (players.length === 0) {
+  if (players?.length === 0) {
     return (
       <div className="d-flex flex-column align-items-center pt-5">
         <Svg src={gamepad} classNames="icon" alt="decoration" />
@@ -37,9 +41,9 @@ const Players = () => {
 
   return (
     <div>
-      <h5>{players.length} {players.length === 1 ? 'player' : 'players'}</h5>
+      <h5>{players?.length} {players?.length === 1 ? 'player' : 'players'}</h5>
       <div className="d-flex flex-column align-items-center pt-2">
-        {players.map((player) => <Player data={player} key={player.id} />)}
+        {players?.map((player) => <Player data={player} key={player.id} />)}
       </div>
     </div>
   );
