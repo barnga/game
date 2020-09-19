97796decbf8f0c487cf5c899766eb3194289ec0d
ssudler <ssudler21@gmail.com>
2020-09-19 19:20:58 -0400

    Update subdomain forwarding

--- src/helpers/baseURL.js
+++ src/helpers/baseURL.js
@@ -1,3 +1,3 @@
-const baseURL = process.env.NODE_ENV === 'production' ? 'https://barnga-api.herokuapp.com/' : 'http://localhost:3000';
+const baseURL = process.env.NODE_ENV === 'production' ? 'http://api.barngaproject.net' : 'http://localhost:3000';
 
 export default baseURL;
