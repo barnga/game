1a8c2342e029fe300425ad5a07b87eca86a3d675
ssudler <ssudler21@gmail.com>
2020-09-20 09:09:18 -0400

    domain name

--- src/helpers/baseURL.js
+++ src/helpers/baseURL.js
@@ -1,3 +1,3 @@
-const baseURL = process.env.NODE_ENV === 'production' ? 'http://barnga-api.barngaproject.com' : 'http://localhost:3000';
+const baseURL = process.env.NODE_ENV === 'production' ? 'http://barnga-api.herokuapp.com' : 'http://localhost:3000';
 
 export default baseURL;
