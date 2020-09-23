6ae367607b54ecc7e17d41f6ffca8fc3d3e8e701
ssudler <ssudler21@gmail.com>
2020-09-23 17:35:21 -0400

    Move CSP headers to express

--- server.js
+++ server.js
@@ -4,11 +4,22 @@ const path = require('path');
 const helmet = require('helmet');
 const cors = require('cors');
 const enforce = require('express-sslify');
+const { expressCspHeader, SELF, NONE } = require('express-csp-header');
 
 const app = express();
 
 if (process.env.NODE_ENV === 'production') {
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(expressCspHeader({
+    directives: {
+      'default-src': [NONE],
+      'connect-src': [SELF, 'https://www.barnga-api.herokuapp.com.net'],
+      'script-src': [SELF],
+      'style-src': [SELF],
+      'font-src': [SELF],
+      'img-src': [SELF],
+    },
+  }));
 }
 
 app.use(cors());
